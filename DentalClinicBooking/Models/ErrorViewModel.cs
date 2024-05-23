066ed378d75bd4205ea3f1f4dc5c7b3e2d3a2031
phucLong <phucvahau157359@gmail.com>
2024-05-23 12:06:51 +0700

    Add project files.

---
+++ DentalClinicBooking/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DentalClinicBooking.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
